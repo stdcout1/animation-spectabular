198ab2d50dab477cc11ca555fc59b7c3325afbce
nasir <nasirkhimani@gmail.com>
2026-08-19 15:00:00 -0400

    more stuff

--- anywidget/elevator/cj.sh
+++ anywidget/elevator/cj.sh
@@ -1,6 +1,11 @@
 uv run jupyter nbconvert --to pdf \
   --execute \
   --ExecutePreprocessor.store_widget_state=True \
+  --TagRemovePreprocessor.enabled=True \
+  --TagRemovePreprocessor.remove_cell_tags='{"remove_cell"}' \
+  --TagRemovePreprocessor.remove_input_tags='{"remove_input", "hidden"}' \
+  --TagRemovePreprocessor.remove_all_outputs_tags='{"remove_output", "hidden"}' \
+  --no-prompt \
   --template acm \
   --TemplateExporter.extra_template_basedirs="../../report/latex_templates" \
   --output Elevator \
